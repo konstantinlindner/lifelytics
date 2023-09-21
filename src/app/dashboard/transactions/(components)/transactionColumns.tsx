c47058a6727c159a98db638e060cb7f9e4019735
Konstantin Lindner <hello@konte.se>
2023-09-21 17:45:00 +0200

    table transactions

---
+++ src/app/dashboard/transactions/(components)/transactionColumns.tsx
@@ -0,0 +1,118 @@
+"use client";
+
+import { ColumnDef } from "@tanstack/react-table";
+import { MoreHorizontal, ArrowUpDown } from "lucide-react";
+
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+  DropdownMenuShortcut,
+} from "@/components/ui/dropdown-menu";
+import { Checkbox } from "@/components/ui/checkbox";
+
+// This type is used to define the shape of our data.
+// You can use a Zod schema here if you want.
+export type Transaction = {
+  date: string;
+  name: string;
+  amount: string;
+  currency: string;
+  country: string;
+};
+
+export const transactionColumns: ColumnDef<Transaction>[] = [
+  {
+    id: "select",
+    header: ({ table }) => (
+      <Checkbox
+        checked={table.getIsAllPageRowsSelected()}
+        onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
+        aria-label="Select all"
+      />
+    ),
+    cell: ({ row }) => (
+      <Checkbox
+        checked={row.getIsSelected()}
+        onCheckedChange={(value) => row.toggleSelected(!!value)}
+        aria-label="Select row"
+      />
+    ),
+    enableSorting: false,
+    enableHiding: false,
+  },
+  {
+    accessorKey: "date",
+    header: "Date",
+  },
+  {
+    accessorKey: "name",
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          Name
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      );
+    },
+  },
+  {
+    accessorKey: "amount",
+    header: () => "Amount",
+    cell: ({ row }) => {
+      const amount = parseFloat(row.getValue("amount"));
+      const currency: string = row.getValue("currency");
+
+      const formatted = new Intl.NumberFormat("en-US", {
+        style: "currency",
+        currency: currency,
+      }).format(amount);
+
+      return <div>{formatted}</div>;
+    },
+  },
+  {
+    accessorKey: "country",
+    header: "Country",
+  },
+  {
+    accessorKey: "currency",
+    header: "Currency",
+  },
+  {
+    id: "actions",
+    cell: ({ row }) => {
+      const transaction = row.original;
+
+      return (
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button
+              variant="ghost"
+              className="flex h-8 w-8 p-0 data-[state=open]:bg-muted"
+            >
+              <MoreHorizontal className="h-4 w-4" />
+              <span className="sr-only">Open menu</span>
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end" className="w-[160px]">
+            <DropdownMenuItem>Edit</DropdownMenuItem>
+            <DropdownMenuItem>Make a copy</DropdownMenuItem>
+            <DropdownMenuItem>Favorite</DropdownMenuItem>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>
+              Delete
+              <DropdownMenuShortcut>⌘⌫</DropdownMenuShortcut>
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      );
+    },
+  },
+];
--- src/app/dashboard/transactions/page.tsx
+++ src/app/dashboard/transactions/page.tsx
@@ -2,9 +2,8 @@ import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
 import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
 
-import { Transaction, columns } from "@/app/dashboard/transactions/columns";
-import { DataTable } from "@/app/dashboard/transactions/dataTable";
-import SignOutButton from "@/app/dashboard/(components)/sign-out-button";
+import { transactionColumns } from "@/app/dashboard/transactions/(components)/transactionColumns";
+import { TransactionTable } from "@/app/dashboard/transactions/(components)/transactionTable";
 
 export default async function Transactions() {
   const supabase = createServerComponentClient({ cookies });
@@ -16,16 +15,34 @@ export default async function Transactions() {
     redirect("/");
   }
 
-  const { data: transactions } = await supabase.from("transactions").select();
+  const { data: transactions } = await supabase.from("transactions").select(`
+  date,
+  name,
+  amount,
+  currency ( code ),
+  country ( name )
+`);
+
+  const transactionsFormatted = transactions?.map((transaction) => ({
+    date: transaction.date,
+    name: transaction.name,
+    amount: transaction.amount,
+    //@ts-ignore
+    currency: transaction.currency.code,
+    //@ts-ignore
+    country: transaction.country.name,
+  }));
 
   return (
     <main>
-      <SignOutButton />
       <h1>Your transactions</h1>
 
       <div className="container mx-auto py-10">
-        {transactions ? (
-          <DataTable columns={columns} data={transactions} />
+        {transactionsFormatted ? (
+          <TransactionTable
+            columns={transactionColumns}
+            data={transactionsFormatted}
+          />
         ) : (
           <p>
             Error, could not load transactions. Please try reloading the page.
