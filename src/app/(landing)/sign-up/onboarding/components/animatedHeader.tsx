1656b525f4c447013bb9b167922b13f796733bfe
Konstantin Lindner <hello@konte.se>
2023-12-18 03:17:25 +0100

    new build

--- src/app/(landing)/sign-up/onboarding/components/animatedHeader.tsx
+++ src/app/(landing)/sign-up/onboarding/components/animatedHeader.tsx
@@ -6,6 +6,7 @@ interface AnimatedHeaderProps {
   firstName: string;
 }
 
+
 export default function AnimatedHeader({ firstName }: AnimatedHeaderProps) {
   return (
     <div className="container flex flex-col space-y-10 items-center">
