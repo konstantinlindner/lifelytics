3bdf1d885aae4dde20fe3b5bd7ecca01253f6823
Konstantin Lindner <hello@konte.se>
2023-12-24 19:24:57 +0100

    update topbar, make client component

--- src/app/dashboard/components/topbar.tsx
+++ src/app/dashboard/components/topbar.tsx
@@ -1,5 +1,6 @@
-import { createServerClient } from '@supabase/ssr';
-import { cookies } from 'next/headers';
+'use client';
+
+import { useUser } from '@/contexts/ProfileContext';
 
 import Link from 'next/link';
 
@@ -21,34 +22,15 @@ import ModeToggle from '@/components/modeToggle';
 import PageHeader from './pageHeader';
 import CommandModal from './commandModal';
 
-export default async function Topbar() {
-  const cookieStore = cookies();
-  const supabase = createServerClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-    {
-      cookies: {
-        get(name: string) {
-          return cookieStore.get(name)?.value;
-        },
-      },
-    },
-  );
+export default function Topbar() {
+  const user = useUser();
 
-  const { data: profiles } = await supabase.from('profiles').select(`
-    avatarUrl,
-    first_name,
-    last_name
-    `);
-  const {
-    data: { session },
-  } = await supabase.auth.getSession();
-
-  const avatarUrl = profiles?.[0].avatarUrl;
-  const fullName = `${profiles?.[0].first_name} ${profiles?.[0].last_name}`;
-  const initials = `${profiles?.[0].first_name?.charAt(
-    0,
-  )}${profiles?.[0].last_name?.charAt(0)}`;
+  const firstName = user?.firstName;
+  const lastName = user?.lastName;
+  const fullName = `${firstName} ${lastName}`;
+  const avatarUrl = user?.avatarUrl ?? '';
+  const email = user?.email;
+  const initials = `${firstName?.charAt(0)}${lastName?.charAt(0)}`;
 
   return (
     <header className="flex justify-between gap-2 items-center py-6 px-10 h-24">
@@ -71,7 +53,7 @@ export default async function Topbar() {
               <div className="flex flex-col space-y-1">
                 <p className="text-sm font-medium leading-none">{fullName}</p>
                 <p className="text-xs leading-none text-muted-foreground">
-                  {session?.user.email}
+                  {email}
                 </p>
               </div>
             </DropdownMenuLabel>
