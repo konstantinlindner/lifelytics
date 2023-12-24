a38050ddb4da73ed70cf7828037b67bd9a84cca6
Konstantin Lindner <hello@konte.se>
2023-12-24 19:21:16 +0100

    add cloudinary upload action

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -5,9 +5,13 @@ import cloudinary from '@/lib/cloudinary';
 export async function CloudinaryBase64ImageUpload(
   image: string,
   options: { folder: string; publicId: string },
-) {
-  cloudinary.uploader.upload(image, {
-    folder: options.folder,
-    public_id: options.publicId,
-  });
+): Promise<string> {
+  return cloudinary.uploader
+    .upload(image, {
+      folder: options.folder,
+      public_id: options.publicId,
+    })
+    .then((result) => {
+      return result.url;
+    });
 }
