f964e5a02382bfe4dea089a42425fb6b91d32888
Konstantin Lindner <hello@konte.se>
2023-12-24 19:26:00 +0100

    supabase

---
+++ src/lib/supabase.ts
@@ -0,0 +1,9 @@
+import { Database } from '@/types/supabase.types';
+import { createBrowserClient } from '@supabase/ssr';
+
+const supabase = createBrowserClient<Database>(
+  process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+);
+
+export default supabase;
