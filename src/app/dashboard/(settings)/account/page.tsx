6ea0c443b0f060df71714434462305ed14d4494a
Konstantin Lindner <hello@konte.se>
2023-12-29 00:27:39 +0100

    populate account page

--- src/app/dashboard/(settings)/account/page.tsx
+++ src/app/dashboard/(settings)/account/page.tsx
@@ -1,7 +1,54 @@
-export default async function Account() {
+'use client';
+
+import SignOutButton from '../../components/signOutButton';
+
+import { Button } from '@/components/ui/button';
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from '@/components/ui/alert-dialog';
+import { toast } from 'sonner';
+
+export default function Account() {
+  function handleAccountDeletion() {
+    toast('Not yet implemented.');
+  }
+
   return (
     <main>
-      <div></div>
+      <div className="flex flex-col">
+        <div className="w-full flex flex-col space-y-4 max-w-xs">
+          <AlertDialog>
+            <AlertDialogTrigger asChild>
+              <Button variant="destructive">Delete account</Button>
+            </AlertDialogTrigger>
+            <AlertDialogContent>
+              <AlertDialogHeader>
+                <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+                <AlertDialogDescription>
+                  This action cannot be undone. This will permanently delete
+                  your account and remove your data from our servers.
+                </AlertDialogDescription>
+              </AlertDialogHeader>
+              <AlertDialogFooter>
+                <AlertDialogCancel>Cancel</AlertDialogCancel>
+                <AlertDialogAction onClick={handleAccountDeletion}>
+                  Continue
+                </AlertDialogAction>
+              </AlertDialogFooter>
+            </AlertDialogContent>
+          </AlertDialog>
+
+          <SignOutButton />
+        </div>
+      </div>
     </main>
   );
 }
