10ae4ef61e946673627cff3b64f9abca9215078c
Konstantin Lindner <hello@konte.se>
2023-12-30 00:17:57 +0100

    improve datepicker

--- src/app/(landing)/onboarding/components/birthDateView.tsx
+++ src/app/(landing)/onboarding/components/birthDateView.tsx
@@ -10,41 +10,83 @@ import {
   PopoverContent,
   PopoverTrigger,
 } from '@/components/ui/popover';
+import { Input } from '@/components/ui/input';
 
 import { Calendar as CalendarIcon } from 'lucide-react';
 
 interface ProfilePictureViewProps {
   currentViewIndex: number;
   setCurrentViewIndex: (view: number) => void;
+  initialDate?: Date;
 }
 
 export default function BirthDateView({
   currentViewIndex,
   setCurrentViewIndex,
+  initialDate,
 }: ProfilePictureViewProps) {
-  const [date, setDate] = useState<Date>();
+  const [date, setDate] = useState<Date | undefined>(initialDate);
+  const [stringDate, setStringDate] = useState(
+    initialDate ? format(initialDate, 'PPP') : '',
+  );
+  const [errorMessage, setErrorMessage] = useState<string>('');
 
   return (
     <section className="w-full space-y-10 flex flex-col items-center">
-      <Popover>
-        <PopoverTrigger asChild>
-          <Button
-            variant={'outline'}
-            className={cn(
-              'w-[280px] justify-start text-left font-normal',
-              !date && 'text-muted-foreground',
-            )}
-          >
-            <CalendarIcon className="mr-2 h-4 w-4" />
-            {date ? format(date, 'PPP') : <span>Pick a date</span>}
-          </Button>
-        </PopoverTrigger>
-        <PopoverContent className="w-auto p-0">
+      <Popover key={date?.getDate()}>
+        <div className="relative w-[280px]">
+          <Input
+            type="string"
+            value={stringDate}
+            onFocus={() => {
+              if (date) setStringDate(format(date, 'MM/dd/yyyy'));
+            }}
+            onChange={(e) => {
+              if (date) setStringDate('');
+              setStringDate(e.target.value);
+            }}
+            onBlur={(e) => {
+              let parsedDate = new Date(e.target.value);
+              if (parsedDate.toString() === 'Invalid Date') {
+                setErrorMessage('Invalid Date');
+              } else {
+                setErrorMessage('');
+                setDate(parsedDate);
+                setStringDate(format(parsedDate, 'PPP'));
+              }
+            }}
+          />
+          {errorMessage !== '' && (
+            <div className="absolute bottom-[-1.75rem] left-0 text-red-400 text-sm">
+              {errorMessage}
+            </div>
+          )}
+          <PopoverTrigger asChild>
+            <Button
+              variant={'outline'}
+              className={cn(
+                'font-normal absolute right-0 translate-y-[-50%] top-[50%] rounded-l-none',
+                !date && 'text-muted-foreground',
+              )}
+            >
+              <CalendarIcon className="w-4 h-4" />
+            </Button>
+          </PopoverTrigger>
+        </div>
+        <PopoverContent align="end" className="w-auto p-0">
           <Calendar
             mode="single"
+            captionLayout="dropdown-buttons"
             selected={date}
-            onSelect={setDate}
-            initialFocus
+            defaultMonth={date}
+            onSelect={(selectedDate) => {
+              if (!selectedDate) return;
+              setDate(selectedDate);
+              setStringDate(format(selectedDate, 'PPP'));
+              setErrorMessage('');
+            }}
+            fromYear={1900}
+            toYear={2024}
           />
         </PopoverContent>
       </Popover>
