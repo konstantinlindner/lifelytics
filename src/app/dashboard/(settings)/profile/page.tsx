1fc61fb7f5ca51b38ea5c04ce2e292339cb780e3
Konstantin Lindner <hello@konte.se>
2023-12-29 01:05:40 +0100

    update styling and add toast

--- src/app/dashboard/(settings)/profile/page.tsx
+++ src/app/dashboard/(settings)/profile/page.tsx
@@ -2,10 +2,13 @@
 
 import { useUser } from '@/contexts/UserContext';
 
+import { toast } from 'sonner';
+
 import ProfilePictureUpload from '@/components/profilePicturePicker';
 
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
+import { Button } from '@/components/ui/button';
 
 export default function Profile() {
   const user = useUser().user;
@@ -14,12 +17,16 @@ export default function Profile() {
   const lastName = user?.lastName ?? '';
   const email = user?.email ?? '';
 
+  function handleSave() {
+    toast('Not yet implemented.');
+  }
+
   return (
     <main>
-      <div className="flex flex-col content-start">
+      <div className="flex flex-col space-y-4 content-start">
         <ProfilePictureUpload />
-        <div className="grid gap-4 py-4">
-          <div className="grid w-full max-w-sm items-center gap-1.5">
+        <div className="grid gap-4 max-w-xs">
+          <div className="grid w-full items-center gap-1.5">
             <Label htmlFor="first-name">First name</Label>
             <Input
               id="first-name"
@@ -27,7 +34,7 @@ export default function Profile() {
               className="col-span-3"
             />
           </div>
-          <div className="grid w-full max-w-sm items-center gap-1.5">
+          <div className="grid w-full items-center gap-1.5">
             <Label htmlFor="last-name">Last name</Label>
             <Input
               id="last-name"
@@ -35,11 +42,14 @@ export default function Profile() {
               className="col-span-3"
             />
           </div>
-          <div className="grid w-full max-w-sm items-center gap-1.5">
+          <div className="grid w-full  items-center gap-1.5">
             <Label htmlFor="email">Email</Label>
             <Input id="email" defaultValue={email} className="col-span-3" />
           </div>
         </div>
+        <Button className="max-w-xs" onClick={handleSave}>
+          Save
+        </Button>
       </div>
     </main>
   );
