3fbdae3bb61db027024f72760ead65dedaed5035
Konstantin Lindner <hello@konte.se>
2024-01-28 01:22:57 +0100

    move transaction db logic into store

--- src/app/(dashboard)/dashboard/transactions/page.tsx
+++ src/app/(dashboard)/dashboard/transactions/page.tsx
@@ -1,52 +1,20 @@
-import { createServerClient } from '@supabase/ssr';
-import { cookies } from 'next/headers';
+'use client';
+
+import { useUser } from '@/contexts/UserContext';
 
 import { transactionColumns } from './components/transactionColumns';
 import { TransactionTable } from './components/transactionTable';
 
-export default async function Transactions() {
-  const cookieStore = cookies();
-  const supabase = createServerClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-    {
-      cookies: {
-        get(name: string) {
-          return cookieStore.get(name)?.value;
-        },
-      },
-    },
-  );
-  const { data: transactions } = await supabase.from('transactions').select(`
-  date,
-  name,
-  amount,
-  currency ( code ),
-  country ( name )
-`);
-
-  const transactionsFormatted = transactions?.map((transaction) => ({
-    date: transaction.date,
-    name: transaction.name,
-    amount: transaction.amount,
-    //@ts-ignore
-    currency: transaction.currency.code,
-    //@ts-ignore
-    country: transaction.country.name,
-  }));
+export default function Transactions() {
+  const { transactions } = useUser();
 
   return (
     <main>
       <div className="mx-auto">
-        {transactionsFormatted ? (
-          <TransactionTable
-            columns={transactionColumns}
-            data={transactionsFormatted}
-          />
+        {transactions ? (
+          <TransactionTable columns={transactionColumns} data={transactions} />
         ) : (
-          <p>
-            Error, could not load transactions. Please try reloading the page.
-          </p>
+          <p>Could not load transactions. Please try reloading the page.</p>
         )}
       </div>
     </main>
