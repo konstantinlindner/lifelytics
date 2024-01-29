fecf86abacbd4c7ae3f85fea9eb8852faf729699
Konstantin Lindner <hello@konte.se>
2024-01-29 15:43:11 +0100

    update db context

--- src/contexts/DatabaseContext.tsx
+++ src/contexts/DatabaseContext.tsx
@@ -16,29 +16,53 @@ type Currencies = {
   id: string;
   name: string | null;
   code: string | null;
+  symbol: string | null;
+  isSymbolPrefix: boolean;
   countries: number | null;
   createdAt: string | null;
   updatedAt: string | null;
 }[];
 
+type Countries = {
+  id: number;
+  name: string | null;
+  localName: string | null;
+  iso2: string;
+  iso3: string | null;
+  continent: string | null;
+}[];
+
 const Context = createContext({
   currencies: null as Currencies | null,
-  fetchCurrencies: () => {},
+  countries: null as Countries | null,
+  fetchDatabaseData: () => {},
 });
 
 export const useDatabase = () => useContext(Context);
 
 export const DatabaseProvider: FC<{ children: ReactNode }> = ({ children }) => {
   const [currencies, setCurrencies] = useState<Currencies | null>();
+  const [countries, setCountries] = useState<Countries | null>();
 
-  const fetchCurrencies = useCallback(async () => {
+  const fetchDatabaseData = useCallback(async () => {
     const { data: currencies } = await supabase.from('currencies').select(`
         id,
         name,
         code,
+        symbol,
+        isSymbolPrefix,
         countries,
         createdAt,
         updatedAt
+    `);
+
+    const { data: countries } = await supabase.from('countries').select(`
+        id,
+        name,
+        localName,
+        iso2,
+        iso3,
+        continent
     `);
 
     if (!currencies) {
@@ -48,6 +72,8 @@ export const DatabaseProvider: FC<{ children: ReactNode }> = ({ children }) => {
         id: currency.id,
         name: currency.name,
         code: currency.code,
+        symbol: currency.symbol,
+        isSymbolPrefix: currency.isSymbolPrefix,
         countries: currency.countries,
         createdAt: currency.createdAt,
         updatedAt: currency.updatedAt,
@@ -55,18 +81,33 @@ export const DatabaseProvider: FC<{ children: ReactNode }> = ({ children }) => {
 
       setCurrencies(formattedCurrencies);
     }
+
+    if (!countries) {
+      setCountries(null);
+    } else {
+      const formattedCountries = countries.map((country) => ({
+        id: country.id,
+        name: country.name,
+        localName: country.localName,
+        iso2: country.iso2,
+        iso3: country.iso3,
+        continent: country.continent,
+      }));
+
+      setCountries(formattedCountries);
+    }
   }, []);
 
   useEffect(() => {
-    fetchCurrencies();
-  }, [fetchCurrencies]);
+    fetchDatabaseData();
+  }, [fetchDatabaseData]);
 
-  if (currencies === undefined) {
+  if (currencies === undefined || countries === undefined) {
     return null;
   }
 
   return (
-    <Context.Provider value={{ currencies, fetchCurrencies }}>
+    <Context.Provider value={{ currencies, countries, fetchDatabaseData }}>
       {children}
     </Context.Provider>
   );
