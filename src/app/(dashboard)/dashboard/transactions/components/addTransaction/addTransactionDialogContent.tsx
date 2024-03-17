9de29e0035eb4658775efcb40c2cd788c792aa2e
Konstantin Lindner <hello@konte.se>
2024-03-17 01:27:57 -0700

    refactor

--- src/app/(dashboard)/dashboard/transactions/components/addTransaction/addTransactionDialogContent.tsx
+++ src/app/(dashboard)/dashboard/transactions/components/addTransaction/addTransactionDialogContent.tsx
@@ -5,57 +5,49 @@ import {
 	DialogTitle,
 } from '@/components/ui/dialog'
 
-import { ScreenType, allScreens } from './addTransactionConstants'
+import { Screen, screens } from './addTransactionConstants'
 import AddTransactionDialogContentButton from './addTransactionDialogContentButton'
 import AddTransactionDialogContentInputs from './addTransactionDialogContentInputs'
 
 interface AddTransactionDialogContentProps {
-	screen: ScreenType
-	setScreen: (newScreen: ScreenType) => void
+	screen: Screen
+	setScreen: (newScreen: Screen) => void
 }
 
 export default function AddTransactionDialogContent({
 	screen,
 	setScreen,
 }: AddTransactionDialogContentProps) {
-	console.log(screen)
-	const currentScreen = allScreens.find((s) => s.screen === screen)
-
-	if (!currentScreen) {
-		return
-	}
-
-	const { title, description } = currentScreen
-	const isEndScreen = !currentScreen.buttonChildren.length
+	const screenChildren = screens.filter(
+		(otherScreen) => otherScreen.parent === screen.id,
+	)
 
 	return (
 		<DialogContent className="max-h-screen max-w-xl overflow-y-scroll">
 			<DialogHeader>
-				<DialogTitle>{title}</DialogTitle>
-				<DialogDescription>{description}</DialogDescription>
+				<DialogTitle>{screen.title}</DialogTitle>
+				<DialogDescription>{screen.subtitle}</DialogDescription>
 			</DialogHeader>
 			<main className="mx-auto py-7">
-				{!isEndScreen && (
+				{screenChildren.length > 0 && (
 					<div
 						className={
-							currentScreen.buttonChildren.length < 3
+							screenChildren.length < 3
 								? 'grid grid-cols-2 gap-4'
 								: 'grid grid-cols-3 gap-4'
 						}
 					>
-						{currentScreen.buttonChildren.map((child, index) => (
+						{screenChildren.map((child, index) => (
 							<AddTransactionDialogContentButton
 								key={index}
-								icon={child.icon}
-								text={child.title}
-								shortcut={String(index + 1)}
+								screen={child}
+								keyboardShortcut={String(index + 1)}
 								setScreen={setScreen}
-								toScreen={child.screen}
 							/>
 						))}
 					</div>
 				)}
-				{isEndScreen && (
+				{!screenChildren.length && (
 					<AddTransactionDialogContentInputs screen={screen} />
 				)}
 			</main>
