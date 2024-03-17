9c94d026b4717f94e3c3c9154eb74daa0ddf8d4a
Konstantin Lindner <hello@konte.se>
2024-03-17 01:24:23 -0700

    add useEffect to root layout

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,19 +1,27 @@
-import { DatabaseProvider } from '@/contexts/DatabaseContext'
-import { UserProvider } from '@/contexts/UserContext'
+'use client'
+
+import { useEffect } from 'react'
+
+import { InitializeStore } from '@/store/StoreHelper'
 
 import { Toaster } from '@/components/ui/sonner'
+
+// todo figure out better way to call SetStore
 
 export default function OuterDashboardLayout({
 	children,
 }: {
 	children: React.ReactNode
 }) {
+	useEffect(() => {
+		console.log('ran SetStore')
+		InitializeStore()
+	}, [])
+
 	return (
-		<UserProvider>
-			<DatabaseProvider>
-				{children}
-				<Toaster />
-			</DatabaseProvider>
-		</UserProvider>
+		<>
+			{children}
+			<Toaster />
+		</>
 	)
 }
