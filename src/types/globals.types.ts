bde9aa3112a105151e7be4a3ac2c6af53c408735
Konstantin Lindner <hello@konte.se>
2024-03-17 01:32:46 -0700

    add types and remove custom types

--- src/types/globals.types.ts
+++ src/types/globals.types.ts
@@ -9,6 +9,8 @@ export type Food = Database['public']['Tables']['food']['Row']
 export type PaymentMethod =
 	Database['public']['Tables']['paymentMethods']['Row']
 export type Profile = Database['public']['Tables']['profiles']['Row']
+export type TransactionCategory =
+	Database['public']['Tables']['transactionCategories']['Row']
 export type Transaction = Database['public']['Tables']['transactions']['Row']
 
 // enums
@@ -17,31 +19,3 @@ export type Continent = Database['public']['Enums']['continents']
 export type FoodPlaceCategory =
 	Database['public']['Enums']['foodPlaceCategories']
 export type FoodTypeCategory = Database['public']['Enums']['foodTypeCategories']
-
-// custom types, need to keep up to date
-export const expenseCategories = [
-	'Home',
-	'Food and drink',
-	'Transportation',
-	'Entertainment',
-	'Health and wellness',
-	'Shopping',
-	'Savings and investments',
-	'Subscriptions',
-	'Other',
-] as const
-
-export type ExpenseCategory = (typeof expenseCategories)[number]
-
-export const incomeCategories = [
-	'Salary',
-	'Sale',
-	'Gift',
-	'Tax return',
-	'Realized investment',
-	'Other income',
-] as const
-
-export type IncomeCategory = (typeof incomeCategories)[number]
-
-export type TransactionCategory = ExpenseCategory | IncomeCategory
