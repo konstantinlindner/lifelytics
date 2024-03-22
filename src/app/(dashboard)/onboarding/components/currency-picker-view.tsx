afa94aebbb4240b39c00c4cdf8115a633e12e25d
Konstantin Lindner <hello@konte.se>
2024-03-22 00:46:23 -0700

    use db and state for currency picker

--- src/app/(dashboard)/onboarding/components/currency-picker-view.tsx
+++ src/app/(dashboard)/onboarding/components/currency-picker-view.tsx
@@ -1,60 +1,78 @@
 import { useState } from 'react'
 
+import { Currency } from '@/types/globals.types'
+
+import { useDatabase } from '@/store/store'
+import { setPrimaryCurrency } from '@/store/store-helper'
+
 import { Badge } from '@/components/ui/badge'
 import { Button } from '@/components/ui/button'
 import { Card } from '@/components/ui/card'
+
+import LoadingIndicator from '@/components/loading-indicator'
 
 interface CurrencyPickerViewProps {
 	currentViewIndex: number
 	setCurrentViewIndex: (view: number) => void
 }
 
-const currencies = ['SEK', 'EUR', 'USD', 'CAD', 'MXN', 'KRW', 'JPY'] as const
-type Currency = (typeof currencies)[number]
-
 export default function CurrencyPickerView({
 	currentViewIndex,
 	setCurrentViewIndex,
 }: CurrencyPickerViewProps) {
-	const [selectedCurrencies, setSelectedCurrencies] = useState<Currency[]>([])
+	const currencies = useDatabase((state) => state.currencies)
+
+	const [isLoading, setIsLoading] = useState(false)
+
+	const [selectedCurrency, setSelectedCurrency] = useState<Currency | null>(
+		null,
+	)
+
+	const handleNextPress = async () => {
+		if (!selectedCurrency) {
+			return
+		}
+
+		setIsLoading(true)
+
+		await setPrimaryCurrency({ primaryCurrency: selectedCurrency })
+
+		setCurrentViewIndex(currentViewIndex + 1)
+	}
 
 	return (
 		<section className="flex w-full flex-col items-center space-y-10">
 			<Card className="flex space-x-1 px-10 py-12">
 				{currencies.map((currency) => (
 					<button
-						key={currency}
+						key={currency.id}
 						onClick={() =>
-							setSelectedCurrencies((current) =>
-								current.includes(currency)
-									? current.filter((x) => x !== currency)
-									: [...current, currency],
+							setSelectedCurrency(
+								selectedCurrency === currency ? null : currency,
 							)
 						}
 					>
 						<Badge
 							className="text-sm"
 							variant={
-								selectedCurrencies.includes(currency)
+								selectedCurrency === currency
 									? 'default'
 									: 'outline'
 							}
 						>
-							{currency}
+							{currency.code}
 						</Badge>
 					</button>
 				))}
 			</Card>
 
 			<Button
-				disabled={selectedCurrencies.length === 0}
-				variant={
-					selectedCurrencies.length === 0 ? 'secondary' : 'default'
-				}
+				disabled={!selectedCurrency}
+				variant={selectedCurrency ? 'default' : 'secondary'}
 				className="w-60"
-				onClick={() => setCurrentViewIndex(currentViewIndex + 1)}
+				onClick={() => handleNextPress()}
 			>
-				Next
+				{isLoading ? <LoadingIndicator size="sm" /> : 'Next'}
 			</Button>
 		</section>
 	)
