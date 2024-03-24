fb7984285f8955a529f533049ba189a7b94b110f
Konstantin Lindner <hello@konte.se>
2024-03-24 01:52:17 -0700

    add city filter

---
+++ src/app/(dashboard)/dashboard/transactions/components/components/city-filter.tsx
@@ -0,0 +1,124 @@
+import { useDatabase } from '@/store/useStore'
+
+import { cn } from '@/lib/utils'
+
+import { Column } from '@tanstack/react-table'
+
+import { CheckIcon, FilterIcon, FilterXIcon } from 'lucide-react'
+
+import { Button } from '@/components/ui/button'
+import {
+	Command,
+	CommandEmpty,
+	CommandGroup,
+	CommandInput,
+	CommandItem,
+	CommandList,
+	CommandSeparator,
+} from '@/components/ui/command'
+import {
+	Popover,
+	PopoverContent,
+	PopoverTrigger,
+} from '@/components/ui/popover'
+
+interface CityFilterProps<TData, TValue> {
+	column?: Column<TData, TValue>
+}
+
+export function CityFilter<TData, TValue>({
+	column,
+}: CityFilterProps<TData, TValue>) {
+	const cities = useDatabase((state) => state.cities)
+
+	const facets = column?.getFacetedUniqueValues()
+
+	const selectedValues = new Set(column?.getFilterValue() as string[])
+
+	return (
+		<Popover>
+			<PopoverTrigger asChild>
+				<Button variant="outline" className="border-dashed">
+					{selectedValues.size > 0 ? (
+						<FilterXIcon className="mr-2 h-4 w-4" />
+					) : (
+						<FilterIcon className="mr-2 h-4 w-4" />
+					)}
+					City
+				</Button>
+			</PopoverTrigger>
+			<PopoverContent className="w-[200px] p-0" align="start">
+				<Command>
+					<CommandInput placeholder="Search category..." />
+					<CommandList>
+						<CommandEmpty>No results found.</CommandEmpty>
+						<CommandGroup>
+							{cities.map((option) => {
+								const isSelected = selectedValues.has(
+									option.englishName,
+								)
+								return (
+									<CommandItem
+										key={option.id}
+										onSelect={() => {
+											if (isSelected) {
+												selectedValues.delete(
+													option.englishName,
+												)
+											} else {
+												selectedValues.add(
+													option.englishName,
+												)
+											}
+											const filterValues =
+												Array.from(selectedValues)
+											column?.setFilterValue(
+												filterValues.length
+													? filterValues
+													: undefined,
+											)
+										}}
+									>
+										<div
+											className={cn(
+												'mr-2 flex h-4 w-4 items-center justify-center rounded-sm border border-primary',
+												isSelected
+													? 'bg-primary text-primary-foreground'
+													: 'opacity-50 [&_svg]:invisible',
+											)}
+										>
+											<CheckIcon
+												className={cn('h-4 w-4')}
+											/>
+										</div>
+										<span>{option.englishName}</span>
+										{facets?.get(option.englishName) && (
+											<span className="ml-auto flex h-4 w-4 items-center justify-center font-mono text-xs">
+												{facets.get(option.englishName)}
+											</span>
+										)}
+									</CommandItem>
+								)
+							})}
+						</CommandGroup>
+						{selectedValues.size > 0 && (
+							<>
+								<CommandSeparator />
+								<CommandGroup>
+									<CommandItem
+										onSelect={() =>
+											column?.setFilterValue(undefined)
+										}
+										className="justify-center text-center"
+									>
+										Clear filter
+									</CommandItem>
+								</CommandGroup>
+							</>
+						)}
+					</CommandList>
+				</Command>
+			</PopoverContent>
+		</Popover>
+	)
+}
