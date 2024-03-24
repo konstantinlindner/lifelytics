53fb2f97753a2c06fd3a538fc78de2c8642e58b2
Konstantin Lindner <hello@konte.se>
2024-03-23 17:35:31 -0700

    rename icons

--- src/components/mode-toggle.tsx
+++ src/components/mode-toggle.tsx
@@ -2,7 +2,7 @@
 
 import { useTheme } from 'next-themes'
 
-import { Moon, SunMedium } from 'lucide-react'
+import { MoonIcon, SunMediumIcon } from 'lucide-react'
 
 import { Button } from '@/components/ui/button'
 
@@ -22,8 +22,8 @@ export default function ModeToggle({ isRound }: ModeToggleProps) {
 			variant="ghost"
 			size="sm"
 		>
-			<SunMedium className="absolute h-5 w-5 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-			<Moon className="absolute h-5 w-5 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+			<SunMediumIcon className="absolute h-5 w-5 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+			<MoonIcon className="absolute h-5 w-5 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
 
 			<span className="sr-only">Toggle theme</span>
 		</Button>
