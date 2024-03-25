c4687f5cc0fdb4aefbb0bf0fac0ad179714cda52
Konstantin Lindner <hello@konte.se>
2024-03-24 20:25:50 -0700

    add delete transaction functionality

--- src/app/(dashboard)/dashboard/transactions/components/transaction-columns.tsx
+++ src/app/(dashboard)/dashboard/transactions/components/transaction-columns.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import { deleteTransaction } from '@/store/store-helper'
 import { Transaction } from '@/store/use-store'
 
 import { ColumnHeader } from '@/app/(dashboard)/dashboard/transactions/components/components/column-header'
@@ -8,6 +9,17 @@ import dayjs from 'dayjs'
 
 import { MoreHorizontalIcon } from 'lucide-react'
 
+import {
+	AlertDialog,
+	AlertDialogAction,
+	AlertDialogCancel,
+	AlertDialogContent,
+	AlertDialogDescription,
+	AlertDialogFooter,
+	AlertDialogHeader,
+	AlertDialogTitle,
+	AlertDialogTrigger,
+} from '@/components/ui/alert-dialog'
 import { Badge } from '@/components/ui/badge'
 import { Button } from '@/components/ui/button'
 import { Checkbox } from '@/components/ui/checkbox'
@@ -130,26 +142,56 @@ export const transactionColumns: ColumnDef<Transaction>[] = [
 			const transaction = row.original
 
 			return (
-				<DropdownMenu>
-					<DropdownMenuTrigger asChild>
-						<Button
-							variant="ghost"
-							className="flex h-8 w-8 p-0 data-[state=open]:bg-muted"
-						>
-							<MoreHorizontalIcon className="h-4 w-4" />
-							<span className="sr-only">Open menu</span>
-						</Button>
-					</DropdownMenuTrigger>
-					<DropdownMenuContent align="end" className="w-[160px]">
-						<DropdownMenuItem>Edit</DropdownMenuItem>
-						<DropdownMenuItem>Duplicate</DropdownMenuItem>
-						<DropdownMenuSeparator />
-						<DropdownMenuItem>
-							Delete
-							<DropdownMenuShortcut>⌘⌫</DropdownMenuShortcut>
-						</DropdownMenuItem>
-					</DropdownMenuContent>
-				</DropdownMenu>
+				<AlertDialog>
+					<DropdownMenu>
+						<DropdownMenuTrigger asChild>
+							<Button
+								variant="ghost"
+								className="flex h-8 w-8 p-0 data-[state=open]:bg-muted"
+							>
+								<MoreHorizontalIcon className="h-4 w-4" />
+								<span className="sr-only">Open menu</span>
+							</Button>
+						</DropdownMenuTrigger>
+						<DropdownMenuContent align="end" className="w-[160px]">
+							<DropdownMenuItem>Edit</DropdownMenuItem>
+							<DropdownMenuItem>Duplicate</DropdownMenuItem>
+
+							<DropdownMenuSeparator />
+
+							<AlertDialogTrigger className="w-full">
+								<DropdownMenuItem>
+									Delete
+									<DropdownMenuShortcut>
+										⌘⌫
+									</DropdownMenuShortcut>
+								</DropdownMenuItem>
+							</AlertDialogTrigger>
+						</DropdownMenuContent>
+					</DropdownMenu>
+
+					<AlertDialogContent>
+						<AlertDialogHeader>
+							<AlertDialogTitle>Are you sure?</AlertDialogTitle>
+							<AlertDialogDescription>
+								This action cannot be undone. This will
+								permanently delete the transaction.
+							</AlertDialogDescription>
+						</AlertDialogHeader>
+						<AlertDialogFooter>
+							<AlertDialogCancel>Cancel</AlertDialogCancel>
+							<AlertDialogAction
+								onClick={() =>
+									deleteTransaction({
+										transactionId: transaction.id,
+									})
+								}
+							>
+								Delete
+							</AlertDialogAction>
+						</AlertDialogFooter>
+					</AlertDialogContent>
+				</AlertDialog>
 			)
 		},
 	},
