17b55422c20a2728f56db7ba3b9fc4f8fee79a67
Konstantin Lindner <hello@konte.se>
2024-03-26 01:46:44 -0700

    change date picker

--- src/app/(dashboard)/dashboard/transactions/components/add-transaction/add-transaction-dialog-content-input.tsx
+++ src/app/(dashboard)/dashboard/transactions/components/add-transaction/add-transaction-dialog-content-input.tsx
@@ -39,6 +39,8 @@ import {
 } from '@/components/ui/popover'
 import { Textarea } from '@/components/ui/textarea'
 
+import DatePicker from '@/components/date-picker'
+
 import { Screen } from './add-transaction-constants'
 
 const FormSchema = z.object({
@@ -235,43 +237,14 @@ export default function AddTransactionDialogContentInput({
 						render={({ field }) => (
 							<FormItem className="flex flex-col">
 								<FormLabel>Date</FormLabel>
-								<Popover>
-									<PopoverTrigger asChild>
-										<FormControl>
-											<Button
-												variant={'outline'}
-												className={cn(
-													'w-[240px] pl-3 text-left font-normal',
-													!field.value &&
-														'text-muted-foreground',
-												)}
-											>
-												{field.value ? (
-													format(field.value, 'PPP')
-												) : (
-													<span>
-														Transaction date
-													</span>
-												)}
-												<CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
-											</Button>
-										</FormControl>
-									</PopoverTrigger>
-									<PopoverContent
-										className="w-auto p-0"
-										align="start"
-									>
-										<Calendar
-											mode="single"
-											selected={field.value}
-											onSelect={field.onChange}
-											disabled={(date) =>
-												date < new Date('1900-01-01')
-											}
-											initialFocus
-										/>
-									</PopoverContent>
-								</Popover>
+								<DatePicker
+									initialDate={new Date()}
+									fromYear={1900}
+									toYear={2024}
+									handleDateChange={(date) => {
+										field.onChange(date)
+									}}
+								/>
 								<FormMessage />
 							</FormItem>
 						)}
