b7bb50ee60a6f9a61346df25bedd79accc66a0e5
Konstantin Lindner <hello@konte.se>
2024-03-28 01:32:58 -0700

    change text in search bar

--- src/app/(dashboard)/dashboard/transactions/components/components/payment-method-filter.tsx
+++ src/app/(dashboard)/dashboard/transactions/components/components/payment-method-filter.tsx
@@ -52,7 +52,7 @@ export function PaymentMethodFilter<TData, TValue>({
 			</PopoverTrigger>
 			<PopoverContent className="w-[200px] p-0" align="start">
 				<Command>
-					<CommandInput placeholder="Search..." />
+					<CommandInput placeholder="Search payment..." />
 					<CommandList>
 						<CommandEmpty>No results found.</CommandEmpty>
 						<CommandGroup>
