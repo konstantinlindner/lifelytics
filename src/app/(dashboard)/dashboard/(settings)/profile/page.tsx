731824e6278371aa5ed15ec4b912462ada06a95c
Konstantin Lindner <hello@konte.se>
2024-04-01 23:31:08 -0700

    add currency and location to settings

--- src/app/(dashboard)/dashboard/(settings)/profile/page.tsx
+++ src/app/(dashboard)/dashboard/(settings)/profile/page.tsx
@@ -6,17 +6,31 @@ import {
 	setBirthDate,
 	setFirstName,
 	setLastName,
+	setLocation,
+	setPrimaryCurrency,
 	setWebsite,
 	updateEmail,
 } from '@/store/store-helper'
-import { useUser } from '@/store/use-store'
+import { useDatabase, useUser } from '@/store/use-store'
+
+import { cn } from '@/lib/utils'
 
 import { zodResolver } from '@hookform/resolvers/zod'
+import dayjs from 'dayjs'
 import { useForm } from 'react-hook-form'
 import { toast } from 'sonner'
 import * as z from 'zod'
 
+import { CheckIcon, ChevronsUpDownIcon } from 'lucide-react'
+
 import { Button } from '@/components/ui/button'
+import {
+	Command,
+	CommandEmpty,
+	CommandGroup,
+	CommandInput,
+	CommandItem,
+} from '@/components/ui/command'
 import {
 	Form,
 	FormControl,
@@ -26,47 +40,61 @@ import {
 	FormMessage,
 } from '@/components/ui/form'
 import { Input } from '@/components/ui/input'
+import {
+	Popover,
+	PopoverContent,
+	PopoverTrigger,
+} from '@/components/ui/popover'
 
 import DatePicker from '@/components/date-picker'
 import LoadingIndicator from '@/components/loading-indicator'
 import ProfilePictureUpload from '@/components/profile-picture-picker'
 
+const formSchema = z.object({
+	firstName: z
+		.string({
+			required_error: 'First name is required',
+		})
+		.trim()
+		.min(2, {
+			message: 'First name should be at least 2 characters long',
+		}),
+	lastName: z
+		.string({
+			required_error: 'Last name is required',
+		})
+		.trim()
+		.min(2, {
+			message: 'Last name should be at least 2 characters long',
+		}),
+	email: z
+		.string({
+			required_error: 'Email is required',
+		})
+		.email(),
+	birthDate: z.date().optional(),
+	website: z.string().trim().optional(),
+	currency: z.string({
+		required_error: 'Please select a currency',
+	}),
+	city: z.coerce.number({
+		required_error: 'Please select a city',
+	}),
+})
+
 export default function Profile() {
 	const [isLoading, setIsLoading] = useState(false)
+
+	const currencies = useDatabase((state) => state.currencies)
+	const cities = useDatabase((state) => state.cities)
 
 	const firstName = useUser((state) => state.firstName)
 	const lastName = useUser((state) => state.lastName)
 	const email = useUser((state) => state.email)
 	const birthDate = useUser((state) => state.birthDate)
 	const website = useUser((state) => state.website)
-
-	const formSchema = z.object({
-		firstName: z
-			.string({
-				required_error: 'First name is required',
-			})
-			.trim()
-			.min(2, {
-				message: 'First name should be at least 2 characters long',
-			}),
-		lastName: z
-			.string({
-				required_error: 'Last name is required',
-			})
-			.trim()
-			.min(2, {
-				message: 'Last name should be at least 2 characters long',
-			}),
-		email: z
-			.string({
-				required_error: 'Email is required',
-			})
-			.email(),
-		birthDate: z.date().optional(),
-		website: z.string().trim().optional(),
-	})
-
-	// todo fix issue with fake empty fields
+	const currency = useUser((state) => state.primaryCurrency)
+	const city = useUser((state) => state.city)
 
 	const form = useForm<z.infer<typeof formSchema>>({
 		defaultValues: {
@@ -75,6 +103,8 @@ export default function Profile() {
 			email: email,
 			birthDate: birthDate ?? undefined,
 			website: website ?? undefined,
+			currency: currency?.id ?? undefined,
+			city: city?.id ?? undefined,
 		},
 		resolver: zodResolver(formSchema),
 	})
@@ -101,6 +131,23 @@ export default function Profile() {
 
 			if (values.website && values.website !== website) {
 				await setWebsite({ website: values.website })
+			}
+
+			const primaryCurrency = currencies.find(
+				(currency) => currency.id === values.currency,
+			)
+			if (
+				primaryCurrency &&
+				values.currency &&
+				values.currency !== currency?.id
+			) {
+				await setPrimaryCurrency({ primaryCurrency })
+			}
+
+			const location = cities.find((city) => city.id === values.city)
+
+			if (location && values.city && values.city !== city?.id) {
+				await setLocation({ city: location })
 			}
 
 			// toast
@@ -146,6 +193,7 @@ export default function Profile() {
 								</FormItem>
 							)}
 						/>
+
 						<FormField
 							control={form.control}
 							name="lastName"
@@ -167,6 +215,7 @@ export default function Profile() {
 								</FormItem>
 							)}
 						/>
+
 						<FormField
 							control={form.control}
 							name="email"
@@ -188,6 +237,7 @@ export default function Profile() {
 								</FormItem>
 							)}
 						/>
+
 						<FormField
 							control={form.control}
 							name="birthDate"
@@ -202,7 +252,7 @@ export default function Profile() {
 													: undefined
 											}
 											fromYear={1900}
-											toYear={2024}
+											toYear={dayjs().year()}
 											{...field}
 											handleDateChange={(date) => {
 												field.onChange(date)
@@ -213,6 +263,7 @@ export default function Profile() {
 								</FormItem>
 							)}
 						/>
+
 						<FormField
 							control={form.control}
 							name="website"
@@ -233,6 +284,153 @@ export default function Profile() {
 								</FormItem>
 							)}
 						/>
+
+						<FormField
+							control={form.control}
+							name="currency"
+							render={({ field }) => (
+								<FormItem className="flex flex-col">
+									<FormLabel>Default currency</FormLabel>
+									<Popover>
+										<PopoverTrigger asChild>
+											<FormControl>
+												<Button
+													variant="outline"
+													role="combobox"
+													className={cn(
+														'w-[200px] justify-between',
+														!field.value &&
+															'text-muted-foreground',
+													)}
+												>
+													{field.value
+														? currencies?.find(
+																(currency) =>
+																	currency.id ===
+																	field.value,
+														  )?.code
+														: 'Select currency'}
+													<ChevronsUpDownIcon className="ml-2 h-4 w-4 shrink-0 opacity-50" />
+												</Button>
+											</FormControl>
+										</PopoverTrigger>
+										<PopoverContent className="w-[200px] p-0">
+											<Command>
+												<CommandInput placeholder="Search currency..." />
+												<CommandEmpty>
+													No currency found.
+												</CommandEmpty>
+												<CommandGroup className="max-h-[20rem] overflow-y-auto">
+													{currencies?.map(
+														(currency) => (
+															<CommandItem
+																value={
+																	currency.name ??
+																	''
+																}
+																key={
+																	currency.id
+																}
+																onSelect={() => {
+																	form.setValue(
+																		'currency',
+																		currency.id ??
+																			'',
+																	)
+																}}
+															>
+																<CheckIcon
+																	className={cn(
+																		'mr-2 h-4 w-4',
+																		currency.id ===
+																			field.value
+																			? 'opacity-100'
+																			: 'opacity-0',
+																	)}
+																/>
+																{currency.code}
+															</CommandItem>
+														),
+													)}
+												</CommandGroup>
+											</Command>
+										</PopoverContent>
+									</Popover>
+									<FormMessage />
+								</FormItem>
+							)}
+						/>
+
+						<FormField
+							control={form.control}
+							name="city"
+							render={({ field }) => (
+								<FormItem className="flex flex-col">
+									<FormLabel>Location</FormLabel>
+									<Popover>
+										<PopoverTrigger asChild>
+											<FormControl>
+												<Button
+													variant="outline"
+													role="combobox"
+													className={cn(
+														'w-[200px] justify-between',
+														!field.value &&
+															'text-muted-foreground',
+													)}
+												>
+													{field.value
+														? cities?.find(
+																(city) =>
+																	city.id ===
+																	field.value,
+														  )?.name
+														: 'Select city'}
+													<ChevronsUpDownIcon className="ml-2 h-4 w-4 shrink-0 opacity-50" />
+												</Button>
+											</FormControl>
+										</PopoverTrigger>
+										<PopoverContent className="w-[200px] p-0">
+											<Command>
+												<CommandInput placeholder="Search city..." />
+												<CommandEmpty>
+													No city found.
+												</CommandEmpty>
+												<CommandGroup className="max-h-[20rem] overflow-y-auto">
+													{cities?.map((city) => (
+														<CommandItem
+															value={
+																city.name ?? ''
+															}
+															key={city.id}
+															onSelect={() => {
+																form.setValue(
+																	'city',
+																	city.id,
+																)
+															}}
+														>
+															<CheckIcon
+																className={cn(
+																	'mr-2 h-4 w-4',
+																	city.id ===
+																		field.value
+																		? 'opacity-100'
+																		: 'opacity-0',
+																)}
+															/>
+															{city.name}
+														</CommandItem>
+													))}
+												</CommandGroup>
+											</Command>
+										</PopoverContent>
+									</Popover>
+									<FormMessage />
+								</FormItem>
+							)}
+						/>
+
 						<div className="pt-6">
 							<Button type="submit">
 								{isLoading ? (
