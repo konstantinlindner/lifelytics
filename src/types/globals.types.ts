5b81c1ab2e9e38ea2ab459505069fb0234268e83
Konstantin Lindner <hello@konte.se>
2024-04-01 22:19:08 -0700

    update types

--- src/types/globals.types.ts
+++ src/types/globals.types.ts
@@ -1,11 +1,14 @@
 import { Database } from '@/types/supabase.types'
 
+// todo add types for all new tables
+
 // tables
 export type City = Database['public']['Tables']['cities']['Row']
 export type Counterpart = Database['public']['Tables']['counterparts']['Row']
 export type Country = Database['public']['Tables']['countries']['Row']
 export type Currency = Database['public']['Tables']['currencies']['Row']
-export type Food = Database['public']['Tables']['food']['Row']
+export type FoodTransaction =
+	Database['public']['Tables']['foodTransactions']['Row']
 export type PaymentMethod =
 	Database['public']['Tables']['paymentMethods']['Row']
 export type Profile = Database['public']['Tables']['profiles']['Row']
@@ -15,7 +18,3 @@ export type Transaction = Database['public']['Tables']['transactions']['Row']
 
 // enums
 export type Continent = Database['public']['Enums']['continents']
-
-export type FoodPlaceCategory =
-	Database['public']['Enums']['foodPlaceCategories']
-export type FoodTypeCategory = Database['public']['Enums']['foodTypeCategories']
