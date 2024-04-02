d8efbac6343f552c6a1784e24e95ca2814e4497a
Konstantin Lindner <hello@konte.se>
2024-04-01 23:18:54 -0700

    add loading state to data fetching

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,22 +1,39 @@
 'use client'
 
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 
 import { InitializeStore } from '@/store/store-helper'
 
 import { Toaster } from '@/components/ui/sonner'
 
-// todo figure out better way to call SetStore
+import LoadingIndicator from '@/components/loading-indicator'
+
+// todo figure out better way to initialize store
 
 export default function OuterDashboardLayout({
 	children,
 }: {
 	children: React.ReactNode
 }) {
+	const [loading, setLoading] = useState(true)
+
 	useEffect(() => {
-		console.log('InitializeStore called from layout.tsx')
-		InitializeStore()
+		async function fetchData() {
+			await InitializeStore()
+			setLoading(false)
+			console.log('InitializeStore called from layout.tsx')
+		}
+
+		fetchData()
 	}, [])
+
+	if (loading) {
+		return (
+			<div className="flex min-h-screen items-center justify-center">
+				<LoadingIndicator size="lg" />
+			</div>
+		)
+	}
 
 	return (
 		<>
