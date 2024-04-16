2c5ade60318df6264a0f232a27bd1337eae717d9
Konstantin Lindner <hello@konte.se>
2024-04-16 14:57:36 -0700

    use env in supabase

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,10 +1,12 @@
+import { env } from '@/env'
+
 import { createBrowserClient } from '@supabase/ssr'
 
 import { Database } from '@/types/supabase.types'
 
 const supabase = createBrowserClient<Database>(
-	process.env.NEXT_PUBLIC_SUPABASE_URL!,
-	process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+	env.NEXT_PUBLIC_SUPABASE_URL,
+	env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
 )
 
 export default supabase
