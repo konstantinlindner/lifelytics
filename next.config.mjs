b6ff1c37e554d42b06aa69e05d54c847613abda2
Konstantin Lindner <hello@konte.se>
2024-04-16 14:56:24 -0700

    import env

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,21 @@
+import createJiti from 'jiti'
+import { fileURLToPath } from 'node:url'
+
+const jiti = createJiti(fileURLToPath(import.meta.url))
+
+/**
+ * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
+ * for Docker builds.
+ */
+jiti('./src/env')
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    experimental: {
-        serverActions: {
-          bodySizeLimit: '8mb'
-        }
-      }
-};
+	experimental: {
+		serverActions: {
+			bodySizeLimit: '8mb',
+		},
+	},
+}
 
-export default nextConfig;
+export default nextConfig
